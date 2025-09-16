2b536fe8125a0db527416e19d4d5980ee4329945
faizahmadnadhif <faizahmadnadhif@gmail.com>
2025-09-16 07:42:34 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Prak MoLa',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Praktikum MoLa"),
+        backgroundColor: Colors.blue,
+      ),
+      body: const Center(
+        child: Text(
+          "Halo, Nama Saya Faiz",   // ganti sesuai nama kamu
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
